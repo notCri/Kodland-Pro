bc2395c66bb4e9c272063f658884738a781f1bba
notCri <154825630+notCri@users.noreply.github.com>
2023-12-25 20:29:40 +0300

    Sozluk.py

---
+++ Sozluk.py
@@ -0,0 +1,21 @@
+meme_dict = {
+    "CRINGE": "Garip ya da utandırıcı bir şey",
+    "LOL": "Komik bir şeye verilen cevap",
+    "ROFL": "Bir şakaya karşılık cevap",
+    "SHEESH": "Onaylamamak",
+    "CREEPY": "Korkunç",
+    "AGGRO": "Agresifleşmek/sinirlenmek"
+}
+
+print("Merhaba! Meme Sözlüğüne hoş geldiniz.")
+print("Aşağıda bazı meme kelimeleri ve anlamları bulunmaktadır.")
+
+for _ in range(5):
+    word = input("\nAnlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+    if word in meme_dict:
+        print(f"{word} - {meme_dict[word]}")
+    else:
+        print(f"{word} sözlükte bulunamadı.")
+
+print("\nTeşekkür ederiz! Meme Sözlüğü'nu kullandığınız için umarız eğlenceli olmuştur.")
